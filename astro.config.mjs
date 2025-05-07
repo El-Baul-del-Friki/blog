2620fd69f0e2f6ea68171e96f45e9cbcfa82b47b
Richard Armuelles <alexanderarmuelles@riseup.net>
2025-05-06 22:14:34 -0500

    fix: astro-compress removed in build time and images compressed

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,13 +9,29 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 
-import compress from 'astro-compress';
+//import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), markdoc(), partytown(), sitemap(), mdx(), compress({
-    Image: true
-  })],
+  integrations: [
+    svelte(),
+    markdoc(),
+    partytown(),
+    sitemap(),
+    mdx(),
+    // Comenta compress antes de hacer un deploy, solo usalo para comprimir las imagenes
+    /*compress({
+      CSS: false,
+      HTML: {
+        "html-minifier-terser": {
+          removeAttributeQuotes: false,
+        },
+      },
+      Image: true,
+      JavaScript: false,
+      SVG: false,
+    }),*/
+  ],
 
   vite: {
     plugins: [tailwindcss()]
