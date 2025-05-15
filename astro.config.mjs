c5f0140121a8e56ce3ed4ef4b375268d0c79d685
Richard Armuelles <alexanderarmuelles@riseup.net>
2025-05-15 16:09:13 -0500

    fix: remove astro-compress from config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,15 +11,13 @@ import mdx from '@astrojs/mdx';
 
 import netlify from '@astrojs/netlify';
 
-import compress from 'astro-compress';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), markdoc(), partytown({
     config: {
       forward: ["dataLayer.push"]
     }
-  }), sitemap(), mdx(), compress()],
+  }), sitemap(), mdx()],
   adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
